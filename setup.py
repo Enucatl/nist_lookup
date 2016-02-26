08abde068e72f4d756a39e657581cd0502a95c45
Matteo Abis <gmatteo..abis@gmail.com>
2016-02-26 11:22:45 +0100

    include db in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
+        '': ['*.db'],
     },
 
     # metadata for upload to PyPI
